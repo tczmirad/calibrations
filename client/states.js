b0af66c2c3b3cd4b12911a6c7cc7ffd543fd678b
Milosz Radziszewski <milosz.radziszewski@flex.com>
2018-01-18 03:38:57 -0800

    [Studio] Add screen "Landing"

--- client/states.js
+++ client/states.js
@@ -33,5 +33,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.landing', {
+        views: {
+            app: {
+                controller: 'app_landing',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.landing');
+                }
+            }
+        }
     });
 });
