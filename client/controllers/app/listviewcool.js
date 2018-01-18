92480eb2f974459c874353d90cd06003eafefd2a
Milosz Radziszewski <milosz.radziszewski@flex.com>
2018-01-18 04:21:25 -0800

    [Studio] Delete screen "app.listviewcool"

--- client/controllers/app/listviewcool.js
+++
@@ -1,5 +0,0 @@
-angular.module('app').controller('app_listviewcool', app_listviewcool);
-function app_listviewcool($scope, app) {
-    'use strict';
-    app.init($scope);
-}
--- client/states.js
+++ client/states.js
@@ -42,14 +42,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.listviewcool', {
-        views: {
-            app: {
-                controller: 'app_listviewcool',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.listviewcool');
-                }
-            }
-        }
     });
 });
