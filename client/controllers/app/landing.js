94d9b78fbf28482c02d839b251369ae7ccd789dd
Milosz Radziszewski <milosz.radziszewski@flex.com>
2018-01-18 06:01:59 -0800

    [Studio] Modified screen "Landing"

--- client/controllers/app/landing.js
+++ client/controllers/app/landing.js
@@ -2,5 +2,6 @@ angular.module('app').controller('app_landing', app_landing);
 function app_landing($scope, app) {
     'use strict';
     app.init($scope);
+    console.log(app);
     app.call("login.logout");
 }
