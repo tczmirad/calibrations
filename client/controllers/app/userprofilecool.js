5dd6dc9c78fa1b13c04858ba89b656f92ae3160c
Milosz Radziszewski <milosz.radziszewski@flex.com>
2018-01-18 06:04:24 -0800

    [Studio] Add screen "UserProfile-Cool"

---
+++ client/controllers/app/userprofilecool.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_userprofilecool', app_userprofilecool);
+function app_userprofilecool($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.userprofilecool', {
+        views: {
+            app: {
+                controller: 'app_userprofilecool',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.userprofilecool');
+                }
+            }
+        }
     });
 });
